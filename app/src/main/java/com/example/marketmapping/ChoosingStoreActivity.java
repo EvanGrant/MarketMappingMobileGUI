1c89def4c1b505c29351c7542d1c5363b51ad8c8
iloveram3n <79485980+iloveram3n@users.noreply.github.com>
2021-12-06 23:56:25 -0500

    Add Empty Home Page and created 2 new buttons on ChoosingStoreActivity page

--- app/src/main/java/com/example/marketmapping/ChoosingStoreActivity.java
+++ app/src/main/java/com/example/marketmapping/ChoosingStoreActivity.java
@@ -3,19 +3,41 @@ package com.example.marketmapping;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.appbar.MaterialToolbar;
 
 public class ChoosingStoreActivity extends AppCompatActivity {
+    private Button button;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choosing_store);
 
+        button = (Button) findViewById(R.id.ChooseStore1Button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHomePage();
+            }
+        });
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
     }
+
+    public void openHomePage()
+    {
+        Intent intent = new Intent(this, HomePage.class);
+        startActivity(intent);
+    }
+
+
 }
