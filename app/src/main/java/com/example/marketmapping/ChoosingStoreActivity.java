5b08104fa41a7e0533c7f4578b6c2996aab17749
EvanGrant <92744387+EvanGrant@users.noreply.github.com>
2022-01-05 20:44:47 -0500

    Added Volley and Gson Implements, as well as INTERNET ACCESS

--- app/src/main/java/com/example/marketmapping/ChoosingStoreActivity.java
+++ app/src/main/java/com/example/marketmapping/ChoosingStoreActivity.java
@@ -3,22 +3,31 @@ package com.example.marketmapping;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.VoiceInteractor;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.appbar.MaterialToolbar;
 
 public class ChoosingStoreActivity extends AppCompatActivity {
     private Button button;
-
+    private RequestQueue mQueue;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choosing_store);
+
+
+
+
+
 
         //This code down here \|/ is syntax for buttons to go to next page
 
