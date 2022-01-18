95b3b168536eaef62cc259aa5f9420dde299fa50
iloveram3n <79485980+iloveram3n@users.noreply.github.com>
2022-01-18 16:02:43 -0500

    Added Network Security Config XML directory and file to get connection, also added the GET Request to the log in page to check connection, it worked. Now onto making an actual log in page

--- app/src/main/java/com/example/marketmapping/MainActivity.java
+++ app/src/main/java/com/example/marketmapping/MainActivity.java
@@ -6,9 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
+
+    Button button;
 
 
 
@@ -17,24 +32,61 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //  syntax for making a button go to the desired page \|/
+        button = findViewById(R.id.SubmitButtonActivityMain); //define new button to the submitbutton button
 
-        button = (Button) findViewById(R.id.SubmitButtonActivityMain);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openChoosingStoreActivity();
+
+                //CODE FOR GET REQUEST, DOWN BELOW GETS EMAIL FROM FIRST INDEX IN THE USERS PAGE, WHICH IS JANE.DOE@EMAIL.COM
+
+                // Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                String url ="http://10.0.2.2:3000/users/";
+
+                JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                        String email = "";
+
+                        try {
+                            JSONObject LogIn = response.getJSONObject(0);
+                            email = LogIn.getString("email");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        Toast.makeText(MainActivity.this, "Email = " + email, Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                queue.add(request);
+//                // Request a string response from the provided URL.
+//                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+//                        new Response.Listener<String>() {
+//                            @Override
+//                            public void onResponse(String response) {
+//                                Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+//                            }
+//                        }, new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        Toast.makeText(MainActivity.this, "Error Occurred", Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+
+                // Add the request to the RequestQueue.
+
+
             }
         });
     }
-
-    public void openChoosingStoreActivity()
-    {
-        Intent intent = new Intent(this, ChoosingStoreActivity.class);
-        startActivity(intent);
-    }
-
-    // syntax for making a button go to the desired page ^
-
 }
 
