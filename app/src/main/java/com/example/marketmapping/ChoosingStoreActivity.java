c7d1880517242014feac4c7d108a0005b723c8cb
iloveram3n <79485980+iloveram3n@users.noreply.github.com>
2022-02-25 15:35:00 -0500

    Finally got everything working. After 6 hours, I fiinally got recyclerview working for ChoosingStoreActivity. Now onto OnClickListener for them.

--- app/src/main/java/com/example/marketmapping/ChoosingStoreActivity.java
+++ app/src/main/java/com/example/marketmapping/ChoosingStoreActivity.java
@@ -2,6 +2,8 @@ package com.example.marketmapping;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.VoiceInteractor;
 import android.content.Intent;
@@ -9,20 +11,73 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.appbar.MaterialToolbar;
 
-public class ChoosingStoreActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
+
+public class ChoosingStoreActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private Adapter mAdapter;
+    private ArrayList<ExampleStoreName> mExampleList;
+    private RequestQueue mRequestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choosing_store);
 
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mExampleList = new ArrayList<>();
+
+        mRequestQueue = Volley.newRequestQueue(this);
+        parseJSON();
     }
 
+    private void parseJSON() {
+        String url = "http://10.0.2.2:3000/stores/";
 
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            for (int i = 0; i < response.length(); i++) {
+                                JSONObject storeObject = response.getJSONObject(i);
+
+                                String storeName = storeObject.getString("name");
+
+                                mExampleList.add(new ExampleStoreName(storeName));
+                            }
+
+                            mAdapter = new Adapter(ChoosingStoreActivity.this, mExampleList);
+                            mRecyclerView.setAdapter(mAdapter);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mRequestQueue.add(jsonArrayRequest);
+
+    }
 }
