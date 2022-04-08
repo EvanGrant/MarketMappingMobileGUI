2d41fd0c99effec80643968e5d74859f4428fc7b
iloveram3n <79485980+iloveram3n@users.noreply.github.com>
2022-04-07 22:19:25 -0400

    Committing ListNameItem.java

---
+++ app/src/main/java/com/example/marketmapping/ListNameItem.java
@@ -0,0 +1,9 @@
+package com.example.marketmapping;
+
+public class ListNameItem {
+    private String mListName;
+
+    public ListNameItem(String listName) { mListName = listName; }
+
+    public String getListName() { return mListName; }
+}
