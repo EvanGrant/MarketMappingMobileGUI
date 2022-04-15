1ddd6e3fd87940723e8136922b65404c35989616
iloveram3n <79485980+iloveram3n@users.noreply.github.com>
2022-04-15 13:18:34 -0400

    only added ShoppingChoseStoreEntrancePage Page, microcommit

---
+++ app/src/main/java/com/example/marketmapping/ShoppingChoseStoreEntrancePage.java
@@ -0,0 +1,14 @@
+package com.example.marketmapping;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShoppingChoseStoreEntrancePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_chose_store_entrance_page);
+    }
+}
